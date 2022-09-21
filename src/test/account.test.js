b97c06bb1e493a034d3819c8a3e089d9f0749ad2
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2022-09-21 21:39:22 +0100

    code refactoring

--- src/test/account.test.js
+++ src/test/account.test.js
@@ -27,20 +27,16 @@ describe('Account', () =>{
 
   it('should show on statement deposit with current balance', () => {
     account.add(100);
-
     expect(account.add(900)).toEqual(" 900.00 deposited. Current balance: 1000.00 ")
-
   });
 
   it('should show on statement withdraw with left balance', () => {
     account.add(300);
-
     expect(account.withdraw(150)).toEqual(" 150.00 withdraw. Current balance: 150.00 ")
   });
 
   it('Should return message if user tries to withdraw more than they have', () => {
     account.add(500);
-
     expect(account.withdraw(1000)).toEqual("Insufficient funds");
   });
 
